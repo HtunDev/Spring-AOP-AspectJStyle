13899d9fda8835127eff6740d17c7cf93d1848c3
hhtun137 <htunaunghlaing199@gmail.com>
2024-03-06 12:30:20 +0630

    writing advices with AspectJStyle

--- src/main/java/com/HAH/demo/aspects/DemoAspects.java
+++ src/main/java/com/HAH/demo/aspects/DemoAspects.java
@@ -1,6 +1,10 @@
 package com.HAH.demo.aspects;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -17,12 +21,39 @@ public class DemoAspects {
 
 	@Before("myServiceBean()")
 	public void beforeInvocation() {
-		System.out.println("This is message of Techinal Concerns");
+		System.out.println("before invocation");
 	}
 
 	@After("myServiceBean()")
 	public void afterInvocation() {
-		System.out.println("This is message of Techinal Concerns");
+		System.out.println("after invocation");
 	}
-	
+
+	@AfterReturning("myServiceBean()")
+	public void afterReturning() {
+		System.out.println("after returning");
+	}
+
+	@AfterThrowing("myServiceBean()")
+	public void afterThrowing() {
+		System.out.println("after throwing");
+	}
+
+	@Around("myServiceBean()")
+	public Object aroundInvocation(ProceedingJoinPoint joinPoint) {
+
+		Object result = null;
+
+		try {
+			System.out.println("Around Befoe Invocation");
+			result = joinPoint.proceed();
+			System.out.println("Around after returning Invocation");
+		} catch (Throwable e) {
+			System.out.println("Around after throwing Invocation");
+			throw new RuntimeException(e);
+		} finally {
+			System.out.println("Around after Invocation");
+		}
+		return result;
+	}
 }
